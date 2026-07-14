9f64e0a21faf3fba63c3ec97e6a053fbf3403ae1
Hemang <saraiyahemang24@gmail.com>
2026-07-14 13:23:23 +0530

    s

--- client/src/Pages/Signup.jsx
+++ client/src/Pages/Signup.jsx
@@ -6,9 +6,11 @@ const Signup = () => {
     const [userName, setUserName] = useState("")
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
+    const [isLoading,setIsLoading]=useState(false)
     const handleSubmit=async (e)=>{
         
         e.preventDefault()
+        setIsLoading(true)
         try{
             await api.post('/auth/signup',{username:userName,email,password,role:"student"})
             navigate('/verifyemail');
@@ -16,6 +18,9 @@ const Signup = () => {
         catch(e){
             window.alert("email already exists")
             console.log(e)
+        }
+        finally{
+          setIsLoading(false)
         }
         setEmail('')
         setPassword('')
@@ -30,14 +35,12 @@ const Signup = () => {
             <input required onChange={(e)=>{setUserName(e.target.value)}} value={userName} type="text" className="bg-gray-100 text-gray-900 border-0 rounded-md p-2 mb-4 focus:bg-gray-200 focus:outline-none focus:ring-1 focus:ring-blue-500 transition ease-in-out duration-150" placeholder="Username" />
           <input required onChange={(e)=>{setEmail(e.target.value)}} value={email} type="email" className="bg-gray-100 text-gray-900 border-0 rounded-md p-2 mb-4 focus:bg-gray-200 focus:outline-none focus:ring-1 focus:ring-blue-500 transition ease-in-out duration-150" placeholder="Email address" />
           <input required onChange={(e)=>{setPassword(e.target.value)}} value={password} type="password" className="bg-gray-100 text-gray-900 border-0 rounded-md p-2 mb-4 focus:bg-gray-200 focus:outline-none focus:ring-1 focus:ring-blue-500 transition ease-in-out duration-150" placeholder="Password" />
-          <div className="flex items-center justify-between flex-wrap">
-            <p className="text-red-500 mt-2">Enter Valid Email (For verification code)</p>
-          </div>
+          
           <div className="flex items-center justify-between flex-wrap">
             <p className="text-gray-900 mt-4">Already have an account? <a href="/login" className="text-sm text-blue-500 -200 hover:underline mt-4">Login</a></p>
           </div>
           
-          <button type="submit" className=" bg-blue-500 text-white font-bold py-2 px-4 rounded-md mt-4 hover:bg-indigo-600 hover:to-blue-600 transition ease-in-out duration-150">Signup</button>
+         <button type="submit" disabled={isLoading} className=" bg-blue-500 text-white font-bold py-2 px-4 rounded-md mt-4 hover:bg-indigo-600 hover:to-blue-600 transition ease-in-out duration-150">{isLoading?('Wait...'):('Sign Up')}</button>
         </form>
       </div>
     </div>
